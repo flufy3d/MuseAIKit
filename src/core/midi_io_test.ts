03950bb934f3e7f12ba23ed36d3369f7814e5256
flufy3d <flufy3d@gmail.com>
2025-04-04 16:08:34 +0200

    pass all unit test

--- src/core/midi_io_test.ts
+++ src/core/midi_io_test.ts
@@ -18,7 +18,7 @@
 import * as fs from 'fs';
 import * as test from 'tape';
 
-import {NoteSequence} from '../protobuf/index';
+import { NoteSequence } from '../protobuf/index';
 
 import * as constants from './constants';
 import * as midi_io from './midi_io';
@@ -27,8 +27,8 @@ import * as sequences from './sequences';
 const simpleNs = NoteSequence.create({
   ticksPerQuarter: 220,
   totalTime: 1.5,
-  timeSignatures: [{time: 0, numerator: 4, denominator: 4}],
-  tempos: [{time: 0, qpm: 120}],
+  timeSignatures: [{ time: 0, numerator: 4, denominator: 4 }],
+  tempos: [{ time: 0, qpm: 120 }],
   sourceInfo: {
     encodingType: NoteSequence.SourceInfo.EncodingType.MIDI,
     parser: NoteSequence.SourceInfo.Parser.TONEJS_MIDI_CONVERT
@@ -182,8 +182,8 @@ const simpleNs = NoteSequence.create({
 const polyNs = NoteSequence.create({
   ticksPerQuarter: 220,
   totalTime: 1.0,
-  timeSignatures: [{time: 0, numerator: 4, denominator: 4}],
-  tempos: [{time: 0, qpm: 120}],
+  timeSignatures: [{ time: 0, numerator: 4, denominator: 4 }],
+  tempos: [{ time: 0, qpm: 120 }],
   sourceInfo: {
     encodingType: NoteSequence.SourceInfo.EncodingType.MIDI,
     parser: NoteSequence.SourceInfo.Parser.TONEJS_MIDI_CONVERT
@@ -229,13 +229,13 @@ const polyNs = NoteSequence.create({
 });
 
 test('Parse Simple MIDI', (t: test.Test) => {
-  const midi = fs.readFileSync('../testdata/melody.mid');
+  const midi = fs.readFileSync('./demos/melody.mid');
   const ns = midi_io.midiToSequenceProto(midi);
 
   t.deepEqual(ns, simpleNs);
 
   const nsRoundTrip = midi_io.midiToSequenceProto(
-      midi_io.sequenceProtoToMidi(ns));
+    midi_io.sequenceProtoToMidi(ns));
 
   t.deepEqual(nsRoundTrip, simpleNs);
 
@@ -246,8 +246,8 @@ test('Create Simple MIDI File', (t: test.Test) => {
   const midiFile = midi_io.sequenceProtoToMidi(simpleNs);
 
   t.deepEqual(
-      midi_io.midiToSequenceProto(midiFile),
-      simpleNs);
+    midi_io.midiToSequenceProto(midiFile),
+    simpleNs);
   t.end();
 });
 
@@ -255,8 +255,8 @@ test('Create MIDI File With Polyphony', (t: test.Test) => {
   const midiFile = midi_io.sequenceProtoToMidi(polyNs);
 
   t.deepEqual(
-      midi_io.midiToSequenceProto(midiFile),
-      polyNs);
+    midi_io.midiToSequenceProto(midiFile),
+    polyNs);
   t.end();
 });
 
@@ -264,9 +264,9 @@ test('Create MIDI File With Tempo Changes', (t: test.Test) => {
   const ns = sequences.clone(simpleNs);
   ns.ticksPerQuarter = 240;  // for precise values
   ns.tempos = [
-    {time: 0, qpm: 80},     // 1 beat  = 0.75 s
-    {time: 0.75, qpm: 480}, // 4 beats = 0.5 s
-    {time: 1.25, qpm: 120}, // 2 beats = 1 s
+    { time: 0, qpm: 80 },     // 1 beat  = 0.75 s
+    { time: 0.75, qpm: 480 }, // 4 beats = 0.5 s
+    { time: 1.25, qpm: 120 }, // 2 beats = 1 s
   ];
 
   const midiFile = midi_io.sequenceProtoToMidi(ns);
@@ -294,7 +294,7 @@ test('Write MIDI Using Defaults', (t: test.Test) => {
   });
 
   const nsRoundTrip = midi_io.midiToSequenceProto(
-      midi_io.sequenceProtoToMidi(strippedNs));
+    midi_io.sequenceProtoToMidi(strippedNs));
 
   t.deepEqual(nsRoundTrip, expectedNs);
 
